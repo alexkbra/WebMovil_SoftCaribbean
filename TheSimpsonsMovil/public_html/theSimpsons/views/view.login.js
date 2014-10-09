3536d175a66e2e065141ad7993c2606c680356c0
alexkbra <alexkbraloco26@gmail.com>
2014-10-09 09:49:20 -0500

    tema de jquery

--- TheSimpsonsMovil/public_html/theSimpsons/views/view.login.js
+++ TheSimpsonsMovil/public_html/theSimpsons/views/view.login.js
@@ -3,5 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$(document).on("pagecreate", "#login", function(event) {
 
+    var header = '';
+    header += '<div data-role="header">';
+    header += '	<div data-role="controlgroup" data-type="horizontal" class="ui-mini ui-btn-right">';
+    header += '	<a href="#" class="ui-shadow ui-btn ui-corner-all ui-btn-icon-left ui-icon-delete ui-btn-b">Cerrar</a>';
+    header += '	</div>';
+    header += '	<h6>l</h6>';
+    header += '</div>';
+
+    var $content = $('<form>'
+            + '	<div>'
+            + '		<label for="correo">Correo:</label>'
+            + '		<input type="email" data-clear-btn="true" name="correo" id="correo" value="" />'
+            + '		<label for="clave">Clave:</label>'
+            + '		<input type="password" data-clear-btn="true" name="clave" id="clave" value="" autocomplete="off" />'
+            + '	</div>'
+            + '	<div>'
+            + '		<a href="#menu" class="ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-icon-left ui-icon-check">Ingresar</a>'
+            + '		<a href="#" class="ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-icon-left ui-icon-claneo ui-btn-b ui-mini">Cancelar</a>'
+            + '		<label for="recordar">Recordar me:</label>'
+            + '		<select name="recordar" id="recordar" data-role="slider">'
+            + '			<option value="no">No</option>'
+            + '			<option value="si">Si</option>'
+            + '		</select>'
+            + '	</div>'
+            + '</form>');
+
+    var footer = '';
+    footer += '<h1>Alex &copy</h1>';
+
+    $(header).appendTo('#header_login');
+    $content.appendTo('#content_login');
+    $(footer).appendTo('#footer_login');
+});
 
